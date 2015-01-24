1e116080ff3ab99e9c4aef4b060c41ec1f7ff2fc
Ryan MacLaptop <ryan2229@hotmail.co.uk>
2015-01-24 11:55:06 +0000

    updated template timer

--- files/template.js
+++ files/template.js
@@ -1,6 +1,7 @@
 //this contains the template functions
 //examples; countdown for the bottom canvas
 
+var x_pos = 0;
 //COUNTDOWN FOR THE UI
 function countownOnTimer() {
   //obtain total time
@@ -10,17 +11,22 @@ function countownOnTimer() {
 	doTimer();
   }
 }
-//INCREMENTALLY MOVE THE BOX BASED ON THE TIMER
-function moveBox(){
-	canvas_context.fillRect(x_pos, 25, 50, 30);
+//UPDATE LOOP BASED ON THE TIMER
+function update(){
+	canvas_context.fillRect(x_pos, 25, 5, 30);
 	x_pos = x_pos + 10;
+	if(x_pos >= ui_canvas.width){
+		alert("Times up");
+		stopTimer();
+	}
 }
 //OUR ACTUAL TIMER
 function doTimer(){
-x_pos = 0;
-timerID = setInterval("moveBox()", 200);
+	timerID = setInterval("update()", 200);
+	update();
 }
-
+//STOP TIMER WHEN THE RECTANGLE HAS HIT THE CANVAS
 function stopTimer(){
 	clearInterval(timerID);
+	window.location.href = "globalgamejam.html";
 }
