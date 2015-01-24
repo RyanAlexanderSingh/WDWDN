3e0e3bcb83750ba5b0cf18f6f708234e48c2b8ab
Ryan MacLaptop <ryan2229@hotmail.co.uk>
2015-01-24 17:03:19 +0000

    updated index.html and started working on zombie equipment layout@

--- files/template.js
+++ files/template.js
@@ -3,15 +3,15 @@
 
 var x_pos = 0;
 //COUNTDOWN FOR THE UI
-function init() {	
+function clock_init() {	
   //get the canvas for the timer
   var timer_canvas = document.getElementById("timer_canvas");
   if(timer_canvas.getContext){
 	canvas_context = timer_canvas.getContext("2d");
 	doTimer();
   }
-  
 }
+
 //UPDATE LOOP
 function update(){
     canvas_context.fillStyle = 'red';
@@ -19,31 +19,44 @@ function update(){
 	x_pos = x_pos + 10;
 	if(x_pos >= timer_canvas.width){
 		alert("Times up");
-		nextLevel();
+		endTimer();
 	}
 }
+
 //OUR ACTUAL TIMER
 function doTimer(){
 	timerID = setInterval("update()", 200);
 	update();
 }
-//STOP TIMER WHEN WE NEED TO PROGRESS TO NEXT LEVEL
-function nextLevel(clicked_id){
+
+//END THE TIMER AND RUN PREPARE
+function endTimer(){
 	clearInterval(timerID);
-	alert(clicked_id);	
-	if(clicked_id == '1'){
-		window.location.href = "equipment.html";
+	prepare();
+}
+
+//STOP TIMER WHEN WE NEED TO PROGRESS TO NEXT LEVEL
+function pickScenario(clicked_id){
+	if(clicked_id == '1')
+	{
+		window.location.href = "zombies/equipment.html";
 	}	
-	if(clicked_id == '2'){
-		window.location.href = "prepare.html";
+	if(clicked_id == '2')
+	{
+		window.location.href = "./prepare.html";
 	}	
-	if(clicked_id == '3'){
-		window.location.href = "outcome.html";
-	}
+	if(clicked_id == '3')
+	{
+		window.location.href = "./outcome.html";
+	}	
 }
 
 function prepare(){
-		window.location.href = "prepare.html";
+	window.location.href = "./prepare.html";
+}
+
+function outcome(){
+	window.location.href = "./outcome.html";
 }
 
 
